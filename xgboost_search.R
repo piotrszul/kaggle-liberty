ea6cda511360f5be53551086deb8693311a3a602
Piotr Szul <piotr.szul@csiro.au>
2015-08-21 14:14:02 +1000

    Di
    to run and history of runs

--- xgboost_search.R
+++ xgboost_search.R
@@ -37,9 +37,9 @@ if (is.na(taskId)) {
     taskId <- 1
 }
 
-nrounds <- 500
-nfold <- 5
-nthread <-3
+nrounds <- 15000
+nfold <- 10 
+nthread <- 16
 
 train_data <- read.csv('data/train.csv')
 set.seed(100)
@@ -53,8 +53,15 @@ storage.mode(X_train_exp) <- "double"
 trainData <- xgb.DMatrix(X_train_exp, label = y_train)
 #commandArgs
 
-paramGrid <- expand.grid(max.depth=c(13,15,17), eta=c(0.01), subsample=c(0.75), colsample_bytree=c(0.5), min_child_weight=c(5,6,7))
-#paramGrid <- expand.grid(max.depth=c(8, 10, 12), eta=c(0.01,0.008), subsample=c(1,0.75,0.5), colsample_bytree=c(1,0.75,0.5))
+#1.paramGrid <- expand.grid(max.depth=c(8, 10, 12), eta=c(0.01,0.008), subsample=c(1,0.75,0.5), colsample_bytree=c(1,0.75,0.5))
+#2.paramGrid <- expand.grid(max.depth=c(1,5,15,30), eta=c(0.1,0.001,0.0001), subsample=c(1, 0.75), colsample_bytree=c(1,0.75,0.5), min_child_weight=c(1,5,10))
+#3.paramGrid <- expand.grid(max.depth=c(5,15,30), eta=c(0.001,0.0001), subsample=c(1, 0.75, 0.5), colsample_bytree=c(1,0.75,0.5), min_child_weight=c(1,5,10))
+#4.paramGrid <- expand.grid(max.depth=c(3,5,10), eta=c(0.005,0.001,0.0005), subsample=c(1, 0.75), colsample_bytree=c(1,0.75), min_child_weight=c(1,10,20))
+#5.paramGrid <- expand.grid(max.depth=c(5,6,7,8,9,10,11,12), eta=c(0.001), subsample=c(0.85, 0.75, 0.65), colsample_bytree=c(1,0.75, 0.6), min_child_weight=c(15,20,25))
+#6.paramGrid <- expand.grid(max.depth=c(8,9,10,11), eta=c(0.001), subsample=c(0.8, 0.75,0.7), colsample_bytree=c(0.75, 0.6,0.5), min_child_weight=c(25,30), gamma=c(0,1))
+#7.paramGrid <- expand.grid(max.depth=c(8,9,10), eta=c(0.001), subsample=c(0.8, 0.75), colsample_bytree=c(0.65, 0.6,0.55), min_child_weight=c(30,35,40), gamma=c(1,5,10))
+paramGrid <- expand.grid(max.depth=c(8,9,10), eta=c(0.001), subsample=c(0.8, 0.75), colsample_bytree=c(0.65, 0.6,0.55), min_child_weight=c(30,35,40), gamma=c(1,5,10))
+
 str(paramGrid)
 taskParams <- as.list(paramGrid[taskId,])
 
@@ -70,16 +77,17 @@ print(taskId)
 print(taskParams)
 
 
-techParams <- list( nthread=4,
+techParams <- list(
+#	     nthread= nthread,
             objective = "reg:linear", 
-            early.stop.round = 10,
-            maximize = TRUE,
+#            early.stop.round = 10,
+#            maximize = TRUE,
             eval_metric = evalerror)
 
 params <- c(taskParams, techParams)
-cv <-xgb.cv(params = params, data = trainData,nrounds = nrounds, nfold = nfold)
-min_nrounds <- which.min(as.numeric(cv$test.rmse.mean))
-result <- data.frame(rmse = as.numeric(cv$test.rmse.mean[min_nrounds]), 
+cv <-xgb.cv(params = params, data = trainData,nrounds = nrounds, nfold = nfold, early.stop.round = 200,  maximize = TRUE, nthread=16)
+min_nrounds <- which.max(as.numeric(cv$test.xxxx.mean))
+result <- data.frame(rmse = as.numeric(cv$test.xxxx.mean[min_nrounds]), 
                     min_nrounds = min_nrounds, taskParams, id=taskId)         
 print(result)
 write.table(result, paste('target/task/out_', taskId, sep=''), row.names=FALSE, quote=FALSE, col.names=FALSE)
